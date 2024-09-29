979cd7d5d179e81a55cab7a84c601f4656dca221
서연 <155455327+seoyeon1123@users.noreply.github.com>
2024-09-29 17:55:07 +0900

    feat : github_start

--- app/(auth)/github/start/route.ts
+++ app/(auth)/github/start/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
-export function GET() {
+export async function GET() {
   const baseURL = 'https://github.com/login/oauth/authorize';
   const params = {
     client_id: process.env.GITHUB_CLIENT_ID!,
